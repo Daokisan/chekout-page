bd3f7626bce0edcf8be1d2d6a306d21d34bd1cc4
Daoki <105397373+Daokisan@users.noreply.github.com>
2022-06-17 11:54:13 -0300

    Add files via upload

---
+++ chekout daoki/src/js/main.js
@@ -0,0 +1,67 @@
+const tsForm = document.querySelector('#myForm');
+const inputs = document.getElementsByTagName('input');
+//
+const alertNotif = document.querySelector('.alert');
+const closeBtn = document.querySelector('.close-btn');
+//
+const btnMinusBag = document.querySelector('.btnMinusBag');
+const counterBagElement =  document.querySelector('.counterBag');
+const btnPlusBag = document.querySelector('.btnPlusBag');
+
+const btnMinusShoes = document.querySelector('.btnMinusShoes');
+const counterShoesElement =  document.querySelector('.counterShoes');
+const btnPlusShoes = document.querySelector('.btnPlusShoes');
+
+
+let counterShoes = 1;
+let counterBag = 1;
+
+tsForm.addEventListener('submit', (event) => {
+	event.preventDefault();
+	const controls = Array.from(inputs);
+	const res = controls.every( (it) => it.value !== '' && it.checkValidity());
+	
+	if (res) {
+		alertNotif.classList.remove('hidden');
+		alertNotif.classList.add("show");
+		alertNotif.classList.remove("hide");
+		alertNotif.classList.add("showAlert");
+		setTimeout(() => {
+		  alertNotif.classList.remove("show");
+		  alertNotif.classList.add("hide");
+		}, 5000);
+	}
+
+}, false);
+
+
+closeBtn.addEventListener("click", function (event) {
+	alertNotif.classList.remove("show");
+	alertNotif.classList.add("hide");
+});
+
+
+
+btnMinusBag.addEventListener('click', event => {
+	event.preventDefault();
+	if (counterBag < 1) { counterBagElement.textContent = '0'; return;}
+
+	counterBagElement.textContent = --counterBag;
+}, false);
+
+btnPlusBag.addEventListener('click', event => {
+	event.preventDefault();
+	counterBagElement.textContent = ++counterBag;
+}, false);
+
+btnMinusShoes.addEventListener('click', event => {
+	event.preventDefault();
+	if (counterShoes < 1) { counterShoesElement.textContent = '0'; return;}
+
+	counterShoesElement.textContent = --counterShoes;
+}, false);
+
+btnPlusShoes.addEventListener('click', event => {
+	event.preventDefault();
+	counterShoesElement.textContent = ++counterShoes;
+}, false);
